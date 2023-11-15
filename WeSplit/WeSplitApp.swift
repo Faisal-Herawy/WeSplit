c943aa2d2fb8087fe5a3636186d081a49551ad06
Faisal Herawy <faisalherawy@icloud.com>
2023-11-15 19:29:40 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Faisal Herawy on 01/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
